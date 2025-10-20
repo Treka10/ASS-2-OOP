47db885297aa67cfc1ed3b99d2aee4f593a2b326
Treka10 <20230020@stud.fci-cu.edu.eg>
2025-10-20 21:19:13 +0300

    Add files via upload

---
+++ PlayerAudio.cpp
@@ -0,0 +1,79 @@
+#include "PlayerAudio.h"
+
+PlayerAudio::PlayerAudio()
+{
+    formatManager.registerBasicFormats();
+
+}
+
+void playerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void playerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void playerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+bool PlayerAudio::loadFile(const juce::File& file)
+{
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            // ?? Disconnect old source first
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset();
+
+            // Create new reader source
+            readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+
+            // Attach safely
+            transportSource.setSource(readerSource.get(),
+                0,
+                nullptr,
+                reader->sampleRate);
+            transportSource.start();
+        }
+    }
+    return true;
+}
+
+void PlayerAudio::start()
+{
+    transportSource.start();
+}
+void PlayerAudio::stop()
+{
+    transportSource.stop();
+}
+
+void PlayerAudio::setGain(float gain)
+{
+    transportSource.setGain(gain);
+}
+
+void PlayerAudio::setPosition(double positionInSeconds)
+{
+    transportSource.setPosition(positionInSeconds);
+}
+
+double PlayerAudio::getPosition() const
+{
+    return transportSource.getCurrentPosition();
+}
+
+double PlayerAudio::getLength() const
+{
+    return transportSource.getLengthInSeconds();
+}
+
+
+
