b2e8c27f36f54cbcf34641d0400be76bde3899fd
Treka10 <20230020@stud.fci-cu.edu.eg>
2025-10-23 17:39:58 +0300

    Add setLooping method to PlayerAudio
    Added setLooping method to PlayerAudio class

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -18,6 +18,7 @@ public:
     void setPosition(double pos);
     double getPosition() const;
     double getLength() const;
+    void setLooping(bool shouldLoop);
 
 private:
     juce::AudioFormatManager formatManager;
