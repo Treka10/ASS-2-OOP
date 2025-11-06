25fbc729bac9157cade3c9fd44b9738c92855114
Treka10 <20230020@stud.fci-cu.edu.eg>
2025-11-06 16:21:28 +0200

    Adjust window size in SimpleAudioPlayer

--- Main.cpp
+++ Main.cpp
@@ -31,7 +31,7 @@ private:
         {
             setUsingNativeTitleBar(true);
             setContentOwned(new MainComponent(), true); // MainComponent = our UI + logic
-            centreWithSize(1000 , 500);
+            centreWithSize(1400 , 700);
             setVisible(true);
         }
 
